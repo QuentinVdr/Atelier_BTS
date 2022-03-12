f76794ad33b0743640273d5fe364f26ad2e72e4a
Quentin Verdier <quent1.verdier@gmail.com>
2022-03-12 15:54:19 +0100

    creation affichage plage et ajout plage

--- Actions/ajout_plage_action.php
+++ Actions/ajout_plage_action.php
@@ -4,13 +4,13 @@ session_start();
 //vérifier le token
 $token=filter_input(INPUT_POST, "token");
 
-if($token!=$_SESSION["token"]){
+if($token!=$_SESSION["token"]) {
     die("On te voit");
 }
 
 //récupration des valeurs
 $nom = filter_input(INPUT_POST, "nom");
-$numero = filter_input(INPUT_POST, "numero");
+$id_c = filter_input(INPUT_POST, "id_commune");
 
 //insertion dans la BDD
 //Je vais chercher dans la config (si pas encore fait)
@@ -18,12 +18,12 @@ require_once "../config.php";
 //Faire une connexion à la BDD
 $pdo = new PDO("mysql:host=" . Config::SERVER . ";dbname=" . Config::BDD, Config::USER, Config::PASSWORD);
 //Préparer la requête
-$requete = $pdo->prepare("INSERT INTO departement(nom, numero) VALUES (:nom, :numero)");
+$requete = $pdo->prepare("INSERT INTO plage(nom, commune_id) VALUES (:nom, :id_c)");
 $requete->bindParam(":nom", $nom);
-$requete->bindParam(":numero", $numero);
+$requete->bindParam(":id_c", $id_c);
 
-$requete->execute();
+//$requete->execute();
 
-//var_dump($nom, $numero);
+var_dump($nom, $id_c, $requete);
 
-header("location: ../edit_departements.php");
+//header("location: ../edit_plages.php");
--- edit_plages.php
+++ edit_plages.php
@@ -12,11 +12,15 @@ if($_SESSION["login_admin"]!=True){
 
 require_once "config.php";
 $pdo = new PDO("mysql:host=" . Config::SERVER . ";dbname=" . Config::BDD, Config::USER, Config::PASSWORD);
-$requete = $pdo->prepare("SELECT * FROM plage INNER JOIN commune ON plage.commune_id = commune.id INNER JOIN departement ON comunne.departement_id = departement.id");
+$requete = $pdo->prepare("SELECT * FROM plage JOIN commune ON plage.commune_id = commune.id JOIN departement ON commune.departement_id = departement.id");
 $requete->execute();
 $plages = $requete-> fetchAll();
 
-var_dump($_SESSION["login_admin"], $plages);
+$requete = $pdo->prepare("SELECT * FROM commune ORDER BY nom");
+$requete->execute();
+$communes = $requete-> fetchAll();
+
+var_dump($_SESSION["login_admin"], $plages, $communes);
 
 $title="edit plages - admin";
 require_once "header.php";
@@ -39,15 +43,33 @@ require_once "navbar.php";
         <?php
         foreach($plages as $p){?>
             <tr>
-                <td><?php echo $p["nom"]; ?></td>
-                <td><?php echo $p["nom"]; ?></td>
-                <td><?php echo $p["nom"]; ?></td>
+                <td><?php echo $p["1"]; ?></td>
+                <td><?php echo $p["4"]; ?></td>
+                <td><?php echo $p["7"]; ?></td>
                 <td></td>
                 <td></td>
             </tr>
         <?php } ?>
         </tbody>
     </table>
+
+    <form action="Actions/ajout_plage_action.php" method="post">
+        <input type="hidden" value="<?php echo $token ?>" name="token" id="token">
+        <div class="form-group">
+            <label for="id_commune">Sellectionner la commune</label>
+            <select name="id_commune" id="id_commune" class="form-control">
+                <option selected disabled>Faites un choix</option>
+                <?php foreach($communes as $c){?>
+                    <option value="<?php echo $c['id']?>"><?php echo $c['nom']?></option>
+                <?php } ?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="nom">Nom de la plage :</label>
+            <input type="text" id="nom" name="nom" class="form-control" required>
+        </div>
+        <input type="submit" class="btn">
+    </form>
 
 </div>
 
